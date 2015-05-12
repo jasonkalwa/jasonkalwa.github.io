fab2bf598c0051455cd0b96ece0d92adce18c120
Andy Bateman <andy@andybateman.com>
2015-05-12 14:47:53 +1200

    Added the wobbles

--- r/javascript/js.js
+++ r/javascript/js.js
@@ -1,4 +1,12 @@
 $(window).load(function(){
-  $("#beginning").css({"margin-top": ($(window).height()/2)-($("#beginning").height()/2)},0).delay(1000).animate({opacity: 1},1000);
+//  $("#beginning").css({"margin-top": ($(window).height()/2)-($("#beginning").height()/2)},0).delay(1000).animate({opacity: 1},1000);
 
+  $(document).mousemove(function(e){
+  	x = Math.round((e.pageX-$(window).width()/2)/$(window).width()*200);
+  	y = Math.round(($(window).height()/2-e.pageY)/$(window).height()*200);
+
+    console.log( x + ", " + y );
+
+    $("#beginning ul li a span").css('background-position',x/10 + "px " + y/10 + "px");
+  });
 });

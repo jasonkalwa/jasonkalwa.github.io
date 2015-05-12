e5938a291e32b32bcf4cb1533c0b5951860b9ff3
Andy Bateman <andy@andybateman.com>
2015-05-12 12:52:48 +1200

    Fixed top margin maths

--- r/javascript/js.js
+++ r/javascript/js.js
@@ -1,7 +1,4 @@
 $(window).load(function(){
-  $("#beginning").css({"margin-top": ($(window).height()/2)-120},0).delay(1000).animate({opacity: 1},1000);
-  $(document).mousemove(function(e){
-    $("#beginning").css('background-position',10-(((e.pageX-($(window).width()/2))/$(window).width())*6));
-    $("#middle").css('background-position',((e.pageX-($(window).width()/2))/$(window).width())*5);
-  });
+  $("#beginning").css({"margin-top": ($(window).height()/2)-($("#beginning").height()/2)},0).delay(1000).animate({opacity: 1},1000);
+
 });
